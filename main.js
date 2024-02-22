399287af6f57ffa4119a7567938782220bab7d30
ZSalah96 <s.e.zerrouki@gmail.com>
2024-02-22 18:47:53 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ main.js
@@ -0,0 +1,10 @@
+function toggleDarkMode() {
+    var body = document.body;
+    var navigation=document.getElementsByClassName('navigation');
+    
+
+    // Toggle dark mode by adding/removing the 'dark-mode' class
+    body.classList.toggle('dark-mode');
+    navigation.classList.toggle('dark-mode');
+  }
+
